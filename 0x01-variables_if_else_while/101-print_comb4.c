24e3db84f1ed5e5a17856c99004ae8eb5ec40ddd
root <komenan.komenan0@gmail.com>
2022-09-09 22:23:11 -0700

    print combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - this program  prints all possible combinations of two-digit
+ * main - this program  prints all possible combinations of tree-digit
  * numbers.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n1, n2;
+	int n1, n2, n3;
 
-	for (n1 = 0; n1 < 9; n1++)
+	for (n1 = 0; n1 < 8; n1++)
 	{
-		for (n2 = n1 + 1; n2 < 10; n2++)
+		for (n2 = n1 + 1; n2 < 9; n2++)
 		{
-			putchar((n1 % 10) + '0');
-			putchar((n2 % 10) + '0');
+			for (n3 = n2 + 1; n3 < 10; n3++)
+			{
+				putchar((n1 % 10) + '0');
+				putchar((n2 % 10) + '0');
+				putchar((n3 % 10) + '0');
 
-			if (n1 == 8 && n2 == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+				if (n1 == 7 && n2 == 8 && n3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
