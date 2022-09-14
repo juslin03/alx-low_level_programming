20d44a40fb5b0fa950fbb9128c6df4996374062e
root <komenan.komenan0@gmail.com>
2022-09-13 20:36:34 -0700

    time table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - function that prints every minute of the day ofi
- * Jack Bauer, starting 
+ * Jack Bauer, starting
  * from 00:00 to 23:59.
  */
 void jack_bauer(void)
@@ -11,13 +11,14 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute <= 50; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
+
 			_putchar('\n');
 		}
 	}
