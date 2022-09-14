e388bc29969a66705d66ba5b6ea1d8a35e437482
root <komenan.komenan0@gmail.com>
2022-09-13 18:56:02 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+/*
+ * Header file for ours program.
+ *
+ * File: main.h
+ * Auth: Juslin Komenan
+ * Desc: Header file that contain declarations of all functions used
+ * in 0x02-function_nested_loops directory
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/* print - _putchar followed by a new line */
+int _putchar(char c);
+
+#endif
