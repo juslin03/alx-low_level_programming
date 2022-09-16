6c90f406d5b2ff7eee6a542b9debcf381b67de32
root <komenan.komenan0@gmail.com>
2022-09-16 05:02:28 -0700

    Print number

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,9 +4,10 @@
  * mul - Function tht multiplies two integers.
  * @a: number 1
  * @b: number 2
- * 
+ *
  * Return: The result.
  */
 int mul(int a, int b)
 {
 	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that prints the numbers
+ * from 0 to 9, followed by a newline.
+ *
+ * Return: the result
+ */
+int print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+		_putchar((i % 10) + '0');
+		i++;
+	_putchar('\n');
+}
