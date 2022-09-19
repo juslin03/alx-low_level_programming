c86fa597499dbadf6ad606bdc9f7ece1127a63ef
root <komenan.komenan0@gmail.com>
2022-09-19 16:29:15 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string followed by
+ * a new line;
+ * @str: string
+ *
+ * Return: no return;
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (cont >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
