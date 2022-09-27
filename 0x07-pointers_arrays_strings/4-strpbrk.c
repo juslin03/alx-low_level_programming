fa0b516518b014d90eca79a602d940dc3c9d5d2a
root <komenan.komenan0@gmail.com>
2022-09-26 21:43:45 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string of any of set bytes
+ * @s: first string
+ * @accept: second tring
+ *
+ * Return: a pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+				return (s + i);
+		}
+	}
+
+	return ('\0');
+}
